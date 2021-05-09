2ae846d0f27df1959822734f0536a8a15280ac3c
Mátyás István Maróty <matyas.maroty@stud.uni-corvinus.hu>
2021-05-09 20:32:12 +0200

    változás

--- HajosTeszt/HajosTeszt/wwwroot/gyak7/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/gyak7/hetedik.js
@@ -1,9 +1,20 @@
 ﻿var kerdesek;
-var sorszam = 0;
+//var sorszam = 0;
+var leptetes = 1;
+var osszesKerdesSzama;
+var aktualisKerdes;
+var hotList = [];
+var questionsInHotList = 3; //Később felkonfiguráljuk 7-re.
+var displayedQuestion; //A hotListből melyik kérdést mutajuk épp
+var numberOfQuestions; //Kérdések száma a teljes adatbázisban -> erre van már nekem egy változóm
+var nextQuestion = 1; //A következő kérdés száma a teljes listában.
+var timeoutHandler;
+var tarolo = window.localStorage;
 
 window.onload = function () {
     letoltes();
-
+    //kerdesBetoltes(leptetes);
+    init();
 };
 
 function letoltes() {
@@ -16,80 +27,99 @@ function letoltesBefejezodott(d) {
     console.log("Sikeres letöltés")
     console.log(d)
     kerdesek = d
-    kerdesMegjelenites(0)
+    //kerdesMegjelenites(0)
 };
 
-function kerdesMegjelenites(kerdes) {
-    document.getElementById("kerdes_szoveg").innerHTML = kerdesek[kerdes].questionText
-    document.getElementById("valasz1").innerHTML = kerdesek[kerdes].answer1
-    document.getElementById("valasz2").innerHTML = kerdesek[kerdes].answer2
-    document.getElementById("valasz3").innerHTML = kerdesek[kerdes].answer3
-    document.getElementById("kep1").src = `https://szoft1.comeback.hu/hajo/${kerdesek[kerdes].image}`
-};
+//fetch('/questions/1')
+//    .then(respons => respons.json())
+//    .then(data => kerdesMegjelenites(data))
 
-function vissza() {
-    if (sorszam != 0) {
-        sorszam = sorszam - 1;
-        kerdesMegjelenites(sorszam)
-    } else {
-        sorszam = 2; kerdesMegjelenites(sorszam)
+fetch('/questions/count')
+    .then(response => response.json())
+    .then(output => {
+        var data = output
+        osszesKerdesSzama = data
     }
-    torles()
+    )
+
+function kerdesMegjelenites() {
+    let kerdes = hotList[displayedQuestion].question;
+    console.log(kerdes);
+    //aktualisKerdes = kerdes
+    document.getElementById("kerdes_szoveg").innerHTML = kerdes.questionText
+    document.getElementById("valasz1").innerHTML = kerdes.answer1
+    document.getElementById("valasz2").innerHTML = kerdes.answer2
+    document.getElementById("valasz3").innerHTML = kerdes.answer3
+    document.getElementById("kep1").src = "https://szoft1.comeback.hu/hajo/" + kerdes.image
 };
 
-function elore() {
-    if (sorszam != 2) {
-        sorszam = sorszam + 1;
-        kerdesMegjelenites(sorszam);
-    } else {
-        sorszam = 0; kerdesMegjelenites(sorszam)
-    }
-    torles()
-};
 
-function klikk() {
-    if (this.innerHTML == "Vissza") {
-        if (sorszam != 0) {
-            sorszam = sorszam - 1;
-            kerdesMegjelenites(sorszam)
-        } else {
-            sorszam = 2; kerdesMegjelenites(sorszam)
-        }
-    } else {
-        if (sorszam != 2) {
-            sorszam = sorszam + 1;
-            kerdesMegjelenites(sorszam);
-        } else { sorszam = 0; kerdesMegjelenites(sorszam) };
-    };
-}
-/*
-function megoldas2(ki) {
-    if (kerdesek[sorszam].correctAnswer == ki) {
-        document.getElementById(`valasz${ki}`).classList.add("jo")
-    } else {
-        document.getElementById(`valasz${ki}`).classList.add("rossz")
-        document.getElementById(`valasz${kerdesek[sorszam].correctAnswer}`).classList.add("jo")
-    }
-}
-*/
-function megoldas(ki) {
-    if (kerdesek[sorszam].correctAnswer == ki) {
-        document.getElementById(`valasz${ki}`).classList.add("jo")
-    } else {
-        for (var i = 1; i < kerdesek.length + 1; i++) {
-            if (kerdesek[sorszam].correctAnswer == i) {
-                document.getElementById(`valasz${kerdesek[sorszam].correctAnswer}`).classList.add("jo")
-            } else {
-                document.getElementById(`valasz${i}`).classList.add("rossz")
+//function kerdesMegjelenites(kerdes) {
+//    console.log(kerdes);
+//    aktualisKerdes = kerdes
+//    document.getElementById("kerdes_szoveg").innerHTML = kerdes.questionText
+//    document.getElementById("valasz1").innerHTML = kerdes.answer1
+//    document.getElementById("valasz2").innerHTML = kerdes.answer2
+//    document.getElementById("valasz3").innerHTML = kerdes.answer3
+//    document.getElementById("kep1").src = "https://szoft1.comeback.hu/hajo/" + kerdes.image
+//};
+
+function kerdesBetoltes(questionNumber, destination) {
+    fetch(`/questions/${questionNumber}`)
+        .then(result => {
+            if (!result.ok) {
+                console.error(`Hibás letöltés: ${response.status}`)
             }
+            else {
+                return result.json()
+            }
+        })
+        .then(
+            q => {
+                hotList[destination].question = q;
+                hotList[destination].goodAnswers = 0;
+                console.log(`A ${questionNumber}. kérdés letöltve a hot list ${destination}. helyére`);
+                if (displayedQuestion == undefined && destination == 0) {
+                    displayedQuestion = 0;
+                    kerdesMegjelenites()
+                }
+            }
+        )
+}
+
+function init() {
+    if (tarolo.getItem('hotListBackup') != null) {
+        var tempList = JSON.parse(tarolo.getItem('hotListBackup'));
+        var tempDisplayed = JSON.parse(tarolo.getItem('displayedQuestionBackup'));
+        var tempNext = JSON.parse(tarolo.getItem('nextQuestionBackup'));
+        hotList = tempList;
+        console.log("Backup betöltve.")
+        displayedQuestion = tempDisplayed;
+        nextQuestion = tempNext;
+        kerdesMegjelenites();
+    } else {
+        for (var i = 0; i < questionsInHotList; i++) {
+            let q = {
+                question: {},
+                goodAnswers: 0
+            }
+            hotList[i] = q;
+        }
+
+        for (var i = 0; i < questionsInHotList; i++) {
+            kerdesBetoltes(nextQuestion, i);
+            nextQuestion++;
         }
     }
-
 }
 
-function torles() {
-    for (let i = 1; i < 4; i++) {
-        document.getElementById(`valasz${i}`).classList.remove("rossz")
-        document.getElementById(`valasz${i}`).classList.remove("jo")
-    }
-}
+
+//function kerdesBetoltes(id) {
+//    fetch(`/questions/${id}`)
+//        .then(response => {
+//            if (!response.ok) {
+//                console.error(`Hibás válasz: ${response.status}`)
+//            }
+//            else {
+//                return response.json()
+//            }
